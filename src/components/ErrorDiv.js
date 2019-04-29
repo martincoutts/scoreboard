dca6430fd583fcc9c5aa9a321a6d5e664cbe3e6d
martincavaliers <martincoutts5@gmail.com>
2019-04-29 20:30:58 +0100

    Conditional rendering of error messages to user now working

--- src/components/ErrorDiv.js
+++ src/components/ErrorDiv.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ErrorDiv = props => (
-  <div id="errorDiv">
-    <h3>{props.standings.errorCode}</h3>
-    <span>{props.standings.message}</span>
-  </div>
-);
+class ErrorDiv extends Component {
+  renderUserMessage() {
+    if (this.props.standings.errorCode === 403) {
+      return (
+        <h3>
+          Apologies, there doesn't seem to be any data on that competition,
+          please select another.
+        </h3>
+      );
+    } else if (this.props.standings.errorCode === 429) {
+      return (
+        <h3>
+          Apologies, you have made too many requests, please try again later.
+        </h3>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div id="errorDiv">
+        <h1>Sorry :(</h1>
+        {this.renderUserMessage()}
+      </div>
+    );
+  }
+}
 
 export default ErrorDiv;
